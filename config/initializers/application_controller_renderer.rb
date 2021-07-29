070ec38a3346f20c9cf7c0a4b1c66ec5dc683151
kishi-risako <calmpanther@docomo.ne.jp>
2021-07-29 14:17:39 +0000

    [Finish]3-10

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '1675e93388dca65d944e03f6f4ed84ce92420a7a05f60422aa7f27954a4d86fed50bbb1edbbdd5fdcca43b920ea0851ee4a980e9d10b644f283f4aeb862baa85'
